3ef1a0730e56ce4cf8eaa7e5eb1cddf7415341c7
Nakarin Saritian <rocdmax@gmail.com>
2023-04-14 15:41:45 +0700

    EDA and model training

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import find_packages,setup
 from typing import List
+
 HYPEN_E_DOT = "-e ."
 def get_requirments(file_path:str)->List[str]:
     """
--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
-import logging
+from src.logger import logging
+
 def error_message_detail(error,error_detail:sys):
     #exc_tb give you specific file and line of error
     _,_,exc_tb = error_detail.exc_info()
