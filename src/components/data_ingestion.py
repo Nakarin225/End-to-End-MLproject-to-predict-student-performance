55e594dc620288b41a7240f4e6cb5a0a329549fd
Nakarin Saritian <rocdmax@gmail.com>
2023-04-15 15:58:34 +0700

    Model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 from src.components.data_tranformation import DataTransformation,DataTransformationConfig
-
+from src.components.model_trainer import ModelTrainerConfig,ModelTrainer
 
 
 @dataclass
@@ -54,3 +54,6 @@ if __name__=="__main__":
 
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    model_trainer=ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr,test_arr))
