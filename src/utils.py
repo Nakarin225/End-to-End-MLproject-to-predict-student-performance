17de411baab5a2582004da06249bc9c11dcc884e
Nakarin Saritian <rocdmax@gmail.com>
2023-04-16 21:00:28 +0700

    Delete app.py

--- src/utils.py
+++ src/utils.py
@@ -6,9 +6,9 @@ import pandas as pd
 import dill
 import pickle
 from sklearn.metrics import r2_score
-from src.exception import CustomException
 from sklearn.model_selection import GridSearchCV
 
+from src.exception import CustomException
 
 def save_object(file_path, obj):
     try:
@@ -17,7 +17,7 @@ def save_object(file_path, obj):
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
-            dill.dump(obj, file_obj)
+            pickle.dump(obj, file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
@@ -56,7 +56,7 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
 def load_object(file_path):
     try:
         with open(file_path, "rb") as file_obj:
-            return dill.load(file_obj)
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
